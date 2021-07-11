5f9a7ceaca723f2846df6fc36bc493de0f0e36ca
yegong322 <yean990830@gmail.com>
2021-07-12 06:53:20 +0900

    Seneir Project

--- grade.js
+++ grade.js
@@ -151,25 +151,25 @@ if (savedToDos) {
 }
 
 // //지역 날씨 API
-const API_KEY = "e1560ad498f4a57603f1d5624019ab59";
+// const API_KEY = "e1560ad498f4a57603f1d5624019ab59";
 
-function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+// function onGeoOk(position) {
+//   const lat = position.coords.latitude;
+//   const lon = position.coords.longitude;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
-}
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const weather = document.querySelector("#weather span:first-child");
+//       const city = document.querySelector("#weather span:last-child");
+//       city.innerText = data.name;
+//       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+//     });
+// }
 
-function ongeoError() {
-  alert("Can't find you. No weather for you.");
-}
+// function ongeoError() {
+//   alert("Can't find you. No weather for you.");
+// }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, ongeoError);
+// navigator.geolocation.getCurrentPosition(onGeoOk, ongeoError);
